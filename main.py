98587d609bd533e3f07c47f10fda7b0e68ec918b
SoggyCereal <PhillipsJo860@northwested.org>
2024-11-06 09:48:58 -0500

    Added content

---
+++ main.py
@@ -0,0 +1,29 @@
+# Joshua Phillips
+# 11/4/24
+# Range Chalenges
+
+import time
+# COUNTING UP AND DOWN:
+# COUNTING UP
+# int = int(input('Put number(integer): '))
+# for i in range(1, int+1):
+#     print(i)
+# COUNTING DOWN
+# for i in range(10, 0, -1):
+#     print(i)
+#     time.sleep(1)
+# print('We have liftoff!')
+
+# NUMBER CUBES
+# for i in range(1, 11, 1):
+#     multiply = i*i*i
+#     print(f'The cube of {i} is = to {multiply}')
+
+# MULTIPLICATION TABLE
+number7 = 7
+for i in range(1, 10, 1):
+    mult = number7 * i
+    print(mult)
+
+# LIST ITERATION
+    
